c2d741f261e4eea72d55738c2e72f957586cb63e
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-01-17 10:38:36 +0530

    Updating comments and formatting and refactoring code.

--- testindex/nuts_and_bolts.py
+++ testindex/nuts_and_bolts.py
@@ -1,17 +1,19 @@
 from datetime import datetime, date
+from dependency_validation import DependencyValidator
 from glob import glob
 from os import environ, path, mkdir, unsetenv, listdir, unlink, devnull, getenv
 from shutil import rmtree
 from subprocess import check_call, CalledProcessError, STDOUT
-
-from yaml import load
-
-from dependency_validation import DependencyValidator
 from summary import Summary
+from yaml import load
 
 
 def execute_command(cmd):
-    """Execute a specified command"""
+    """
+    Execute a specified command
+    Returns true if commands runs successfully, else returns False
+    :param cmd The command to execute passed as a list, each parameter, as one item
+    """
     try:
         fnull = open(devnull, "w")
         check_call(cmd, stdout=fnull, stderr=STDOUT)
@@ -20,11 +22,29 @@ def execute_command(cmd):
         return False
 
 
+def cleanup_content(folder, del_sub_dirs=True):
+    for current_file in listdir(folder):
+
+        file_path = path.join(folder, current_file)
+
+        try:
+            if path.isfile(file_path):
+                unlink(file_path)
+
+            elif del_sub_dirs and path.isdir(file_path):
+                rmtree(file_path)
+
+        except Exception as e:
+            print e
+
+
 class Environment(object):
     """Handles the bringing up and teardown of the test environment"""
 
     def __init__(self):
-
+        """
+        Initialize the Environment object to manage the environment.
+        """
         home = getenv("HOME")
         if home:
             dump_location = home + "/cccp-index-test"
@@ -48,7 +68,8 @@ class Environment(object):
         self.old_environ = dict(environ)
 
         # Create all test bench directory structure
-        for item in [self.dump_directory, self.index_test_bench, self.index_dir, self.test_index, self.repo_dump,
+        for item in [self.dump_directory, self.index_test_bench, self.index_dir,
+                     self.test_index, self.repo_dump,
                      generator_location, self.generator_dir]:
             try:
                 if not path.exists(item):
@@ -60,48 +81,35 @@ class Environment(object):
         unsetenv("GIT_ASKPASS")
         unsetenv("SSH_ASKPASS")
 
-    @staticmethod
-    def _cleanup_content(folder, del_sub_dirs=True):
-        """Wipes a directoy clean without deleting the directory"""
-        for current_file in listdir(folder):
-
-            file_path = path.join(folder, current_file)
-
-            try:
-                if path.isfile(file_path):
-                    unlink(file_path)
-
-                elif del_sub_dirs and path.isdir(file_path):
-                    rmtree(file_path)
-
-            except Exception as e:
-                print e
-
     def cleanup_index_testbench(self, full=False):
         """Clean up the test bench directory"""
         if full:
-            self._cleanup_content(self.index_test_bench)
+            cleanup_content(self.index_test_bench)
         else:
             removables = glob(self.index_test_bench + "/*")
             for removable in removables:
                 removable_name = path.basename(removable)
                 removable_date_str = removable_name.split("_")[0]
-                removeable_date = datetime.strptime(removable_date_str, "%Y-%m-%d").date()
+                removable_date = datetime.strptime(removable_date_str,
+                                                    "%Y-%m-%d").date()
                 current_date = date.today()
-                if removeable_date < current_date:
-                    self._cleanup_content(removable)
+                if removable_date < current_date:
+                    cleanup_content(removable)
                     rmtree(removable)
 
     def teardown(self, cleanup_test_bench=False):
         """Tear down the environment"""
         environ.update(self.old_environ)
         if cleanup_test_bench:
-            self._cleanup_content(self.dump_directory)
+            cleanup_content(self.dump_directory)
         else:
             self.cleanup_index_testbench()
 
 
 class Context(object):
+    """
+    This class acts as a context for each run of the index ci.
+    """
     def __init__(self):
         self.environment = Environment()
         self.summary = Summary(self.environment.summary_location)
@@ -109,6 +117,9 @@ class Context(object):
 
 
 class StatusIterator(object):
+    """
+    This class allows you to iterate over index ci status information.
+    """
     def __init__(self, generator_ref_dir):
         self._current = -1
         self._src = glob(generator_ref_dir + "/*.yml")
